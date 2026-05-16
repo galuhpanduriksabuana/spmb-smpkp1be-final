d2746cc40a7d00346a3dfbc724d37314b35363b0
Galuh P.B <32071760+galuhpanduriksabuana@users.noreply.github.com>
2026-05-17 01:07:03 +0700

    Set GAS_WEB_APP_URL to the deployed script URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 // Service to interact with Google Apps Script Backend
 
 // To use the real backend, replace this URL with your deployed Google Apps Script Web App URL
-const GAS_WEB_APP_URL = ""; 
+const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwpAdl9czIdHPKSKxbM3R1Ztph2TuQ_M5wslTzpLIjCWNC82gwwxw9VXd9c9DtcEOpF/exec"; 
 
 export interface FormField {
   id: string;
